9f8c0c886e8a00e826a95fd1e017a5bdbf9d9321
John Barton (joho) <jrbarton@gmail.com>
2009-05-20 17:24:08 +1000

    get started on trying to tag already printed index cards add another page that will only show specific tickets

--- lighthouse-cards.rb
+++ lighthouse-cards.rb
@@ -4,10 +4,27 @@ require 'hpricot'
 require 'open-uri'
 
 class Ticket
-  attr_reader :id, :title
+  attr_reader :id, :title, :tags
   
-  def initialize(id, title)
-    @id, @title = id, title
+  def initialize(id, title, tags)
+    @id, @title, @tags = id, title, tags
+  end
+  
+  def mark_printed!
+    project_key, account_name, token = Ticket.config['project_key'], Ticket.config['account_name'], Ticket.config['auth_token']
+
+    new_tags = "card-printed " + @tags.to_s
+
+    uri = URI.parse("http://#{account_name}.lighthouseapp.com/projects/#{project_key}/tickets/#{id}.xml?_token=#{token}")
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      headers = {'Content-Type' => 'text/plain; charset=utf-8'}
+      put_data = "<ticket><tag>#{new_tags}</tag></ticket>"
+      response = http.send_request('PUT', uri.request_uri, put_data,
+    headers)
+    
+      p response
+    end
+    
   end
   
   def self.config
@@ -16,7 +33,7 @@ class Ticket
   
   def self.ticket_doc(page_num = 1)
     project_key, account_name, token = config['project_key'], config['account_name'], config['auth_token']
-    url = "http://#{account_name}.lighthouseapp.com/projects/#{project_key}/tickets.xml?q=state:open&_token=#{token}&page=#{page_num}"
+    url = "http://#{account_name}.lighthouseapp.com/projects/#{project_key}/tickets.xml?q=state:open%20not-tagged:card-printed&_token=#{token}&page=#{page_num}"
 
     Hpricot(open(url))
   end
@@ -28,7 +45,7 @@ class Ticket
       return []
     else
       doc.collect do |ticket|
-        Ticket.new((ticket/'number').inner_text, (ticket/'title').inner_text)
+        Ticket.new((ticket/'number').inner_text, (ticket/'title').inner_text, (ticket/'tag').inner_text)
       end + all(current_page + 1)
     end
   end
@@ -48,4 +65,20 @@ end
 get '/' do
   @tickets = Ticket.all
   haml :index
+end
+
+post '/print_specific' do
+  ticket_ids = params[:ticket_ids].split(',').collect {|id| id.to_i}
+  @tickets = Ticket.all.select {|ticket| ticket_ids.include?(ticket.id.to_i)}
+  
+  haml :index
+end
+
+post '/mark_printed' do
+  tickets = Ticket.all
+  tickets.each do |ticket|
+    ticket.mark_printed!
+  end
+  
+  "blah!"
 end
